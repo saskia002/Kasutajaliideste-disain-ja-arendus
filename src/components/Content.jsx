94e973c6842a8843de4fe80c0e6d6276800ada60
Rain Erik Elias <90192521+RainErikE@users.noreply.github.com>
2023-04-24 17:28:24 +0300

    artiklid

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,45 +2,8 @@ import "../styles/content.css";
 
 export default function Content() {
 	return (
-		<div className="content-container">
-			<main className="main-body">
-
-				<div className="vector-page-toolbar">
-					<div className="vector-page-toolbar-container">
-						<div id="left-navigation">
-							<nav aria-label="Namespaces">
-								<div className="vector-menu vector-menu-tabs mw-portlet mw-portlet-associated-pages">
-									<div className="vector-menu-content">
-										<ul className="vector-menu-content-list">
-											<li className="selected vector-tab-noicon mw-list-item">
-												<a className="" data-mv="interface" href="">
-													<span>Main Page</span>
-												</a>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</nav>
-						</div>
-						<div id="right-navigation" className="vector-collapsible">
-							<nav aria-label="Views">
-								<div className="vector-menu vector-menu-tabs mw-portlet mw-portlet-views">
-									<div className="vector-menu-content">
-										<ul className="vector-menu-content-list">
-											<li className="selected vector-tab-noicon mw-list-item">
-												<a className="" data-mw="interface" href="">
-													<span>Read</span>
-												</a>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</nav>
-						</div>
-					</div>
-				</div>
-
-				<div id="bodyContent" className="vector-body">
+		<main className="main-body">
+			<div id="bodyContent" className="vector-body">
 				<div className="content-text">
 					<div>
 						<div id="mp-bottom" className="mp-box normak-body">
@@ -57,10 +20,53 @@ export default function Content() {
 							</div>
 						</div>
 					</div>
+					<div>
+						<div id="mp-bottom" className="mp-box villem-body">
+							<h2 id="mp-other" className="mp-h2 villem-header">
+								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
+									Villem Nano
+								</span>
+							</h2>
+							<div id="mp-other-content">
+								<figure>
+									<img src="/src/resources/media/Villem_Nano.jpg" alt="" width="150" />
+									<figcaption>Villem Nano</figcaption>
+								</figure>
+								<p>
+									Täna 100 aastat tagasi - Villem Nano valiti Tartu Ülikooli Üliõpilaskonna Esinduse esimeheks - Nano oli laia kogemuse ja
+									väga mitme töökohaga akadeemik. Ta õpetas oma eluaja jooksul väga mitmes Eesti koolis, eelkõige geomeetriat, algebrat ja
+									kosmograafiat. Ta oli õppinud ka Saksamaa tähtsates ülikoolides ning oli kokkuvõttes üks eeskujulik näide tudengist. Tema
+									kõige tähtsam roll oli Eesti ühiskonnas aga õpetajate õpetajana. Tema panused õpetajate seltsidesse ja Tallinna Õpetajate
+									Seminari jättis Eestisse pikaks ajaks tugeva õpetajate kultuuri. Ta on Peeter Normaku emapoolne vanaisa.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div id="mp-bottom" className="mp-box ratas-body">
+							<h2 id="mp-other" className="mp-h2 ratas-header">
+								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
+									Andrus ja Kalle ratastel
+								</span>
+							</h2>
+							<div id="mp-other-content">
+								<figure>
+									<img src="/src/resources/media/Kalle_ja Andrus_ratastel.jpg" alt="" width="150" />
+									<figcaption>Andrus ja Kalle</figcaption>
+								</figure>
+								<p>
+									Tallinna Ülikool asub suurlinna liikluse südames. Seega on asutus pidanud algusest saadik väga tähtsaks ligipääsetavust ning
+									alternatiive autodele. Pildil on näha kahte TLÜ õppejõudu, Andrus Rinde ning Kalle Kivi. Mõlemad on Digitehnoloogiate
+									Instituudis tuntud õppejõud ning lisaks rattaga liikumise suured sõbrad. Kalle Kivi korraldab regulaarselt tudengitega ja
+									teiste õppejõududega rattamatkasid ning Andrus Rinde on entusiastlikult alati kaasas. Tallinna Ülikool asub Tallinna ühe
+									kõige liigelduma tänava - Narva maantee ääres. Kooli ümber on parkimine tasuline ning paljud tudengid liiguvad kooli jalgsi
+									või ühistranspordiga. Seega on koolil hea positsioon reklaamida efektiivsemaid liikumismeetodeid.
+								</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-
-			</main>
-		</div>
+		</main>
 	);
 }
