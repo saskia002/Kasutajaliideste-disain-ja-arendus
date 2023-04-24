04742fbdf1aee4e7e637cabd3d60cebcb8477301
Siim Kriibi <siim02@tlu.ee>
2023-04-24 17:04:53 +0300

    content box

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,25 @@
+import "../styles/content.css";
 export default function Content() {
 	return (
 		<main className="main-body">
 			<div id="bodyContent" className="vector-body">
-				<div className="content-text">asd</div>
+				<div className="content-text">
+					<div>
+						<div id="mp-bottom" className="mp-box normak-body">
+							<h2 id="mp-other" className="mp-h2 normak-header">
+								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
+									Peeter Normak
+								</span>
+							</h2>
+							<div id="mp-other-content">
+								<figure>
+									<img src="/src/resources/media/petja_bling_bling.png" alt="" width="150" />
+									<figcaption>Peeter Normak TLÜ hoones</figcaption>
+								</figure>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	);
