8c214c198b545fa33751d570450a00a50ee46b93
Siim Kriibi <siim02@tlu.ee>
2023-04-24 18:11:51 +0300

    done?

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ export default function Content() {
 		<main className="main-body">
 			<div id="bodyContent" className="vector-body">
 				<div className="content-text">
-					<div>
+					<div className="villem-ja-normak-wrap">
 						<div id="mp-bottom" className="mp-box normak-body">
 							<h2 id="mp-other" className="mp-h2 normak-header">
 								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
@@ -16,7 +16,7 @@ export default function Content() {
 								<div class="img-wrap">
 									<figure>
 										<img src="/src/resources/media/petja_bling_bling.png" alt="" width="150" />
-										<figcaption>Peeter Normak TLÜ hoones, 2020</figcaption>
+										<figcaption>Peeter Normak TLÜ hoones, TLÜ, 2020</figcaption>
 									</figure>
 								</div>
 								<p>
@@ -36,12 +36,11 @@ export default function Content() {
 								<p>Tema emapoolne vanaisa oli eesti matemaatik, pedagoog ja koolijuht Villem Nano.</p>
 							</div>
 						</div>
-					</div>
-					<div>
+
 						<div id="mp-bottom" className="mp-box villem-body">
 							<h2 id="mp-other" className="mp-h2 villem-header">
 								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
-									Villem Nano
+									Did you know ...
 								</span>
 							</h2>
 							<div id="mp-other-content">
@@ -50,11 +49,24 @@ export default function Content() {
 									<figcaption>Villem Nano</figcaption>
 								</figure>
 								<p>
-									Täna 100 aastat tagasi - Villem Nano valiti Tartu Ülikooli Üliõpilaskonna Esinduse esimeheks - Nano oli laia kogemuse ja
-									väga mitme töökohaga akadeemik. Ta õpetas oma eluaja jooksul väga mitmes Eesti koolis, eelkõige geomeetriat, algebrat ja
-									kosmograafiat. Ta oli õppinud ka Saksamaa tähtsates ülikoolides ning oli kokkuvõttes üks eeskujulik näide tudengist. Tema
-									kõige tähtsam roll oli Eesti ühiskonnas aga õpetajate õpetajana. Tema panused õpetajate seltsidesse ja Tallinna Õpetajate
-									Seminari jättis Eestisse pikaks ajaks tugeva õpetajate kultuuri. Ta on Peeter Normaku emapoolne vanaisa.
+									<p>
+										<b>Villem Nano (Naano) </b> (6. juuli 1893 Vana-Nursi vald (Rõuge kihelkond) Võrumaa – 10. veebruar 1965 Tallinn) oli
+										eesti matemaatik, pedagoog ja koolijuht.
+									</p>
+									<p>
+										Täna 100 aastat tagasi - Villem Nano valiti Tartu Ülikooli Üliõpilaskonna Esinduse esimeheks - Nano oli laia kogemuse ja
+										väga mitme töökohaga akadeemik. Ta õpetas oma eluaja jooksul väga mitmes Eesti koolis, eelkõige geomeetriat, algebrat ja
+										kosmograafiat. Ta oli õppinud ka Saksamaa tähtsates ülikoolides ning oli kokkuvõttes üks eeskujulik näide tudengist.
+									</p>
+									<p>
+										Tema kõige tähtsam roll oli Eesti ühiskonnas aga õpetajate õpetajana. Tema panused õpetajate seltsidesse ja Tallinna
+										Õpetajate Seminari jättis Eestisse pikaks ajaks tugeva õpetajate kultuuri.
+									</p>
+									<p>Ta on Peeter Normaku emapoolne vanaisa.</p>
+									<p>
+										Villem Nano oli talupidaja poeg. Tema koolitee algas 1900. aastal Vana-Nursi vallakoolis ning jätkus 1903. aasta sügisel
+										Rõuge kihelkonnakoolis.
+									</p>
 								</p>
 							</div>
 						</div>
@@ -63,21 +75,30 @@ export default function Content() {
 						<div id="mp-bottom" className="mp-box ratas-body">
 							<h2 id="mp-other" className="mp-h2 ratas-header">
 								<span id="Other_areas_of_Wikipedia" className="mw-headline ">
-									Andrus ja Kalle ratastel
+									Today's featured picture
 								</span>
 							</h2>
 							<div id="mp-other-content">
 								<figure>
-									<img src="/src/resources/media/Kalle_ja Andrus_ratastel.jpg" alt="" width="150" />
-									<figcaption>Andrus ja Kalle</figcaption>
+									<img src="/src/resources/media/Kalle_ja Andrus_ratastel.jpg" alt="" className="rata-fännid" />
+									<figcaption>Andrus Rinde ja Kalle Kivi, TLÜ, 2021</figcaption>
 								</figure>
 								<p>
 									Tallinna Ülikool asub suurlinna liikluse südames. Seega on asutus pidanud algusest saadik väga tähtsaks ligipääsetavust ning
-									alternatiive autodele. Pildil on näha kahte TLÜ õppejõudu, Andrus Rinde ning Kalle Kivi. Mõlemad on Digitehnoloogiate
-									Instituudis tuntud õppejõud ning lisaks rattaga liikumise suured sõbrad. Kalle Kivi korraldab regulaarselt tudengitega ja
-									teiste õppejõududega rattamatkasid ning Andrus Rinde on entusiastlikult alati kaasas. Tallinna Ülikool asub Tallinna ühe
-									kõige liigelduma tänava - Narva maantee ääres. Kooli ümber on parkimine tasuline ning paljud tudengid liiguvad kooli jalgsi
-									või ühistranspordiga. Seega on koolil hea positsioon reklaamida efektiivsemaid liikumismeetodeid.
+									alternatiive autodele.
+								</p>
+								<p>
+									Pildil on näha kahte TLÜ õppejõudu, Andrus Rinde ning Kalle Kivi. Mõlemad on Digitehnoloogiate Instituudis tuntud õppejõud
+									ning lisaks rattaga liikumise suured sõbrad.
+								</p>
+								<p>
+									Kalle Kivi korraldab regulaarselt tudengitega ja teiste õppejõududega rattamatkasid ning Andrus Rinde on entusiastlikult
+									alati kaasas.
+								</p>
+								<p>
+									Tallinna Ülikool asub Tallinna ühe kõige liigelduma tänava - Narva maantee ääres. Kooli ümber on parkimine tasuline ning
+									paljud tudengid liiguvad kooli jalgsi või ühistranspordiga. Seega on koolil hea positsioon reklaamida efektiivsemaid
+									liikumismeetodeid.
 								</p>
 							</div>
 						</div>
