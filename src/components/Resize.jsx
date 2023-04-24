bbfb78dbe97fd842bae21195fa105ac2474f24a7
Siim Kriibi <siim02@tlu.ee>
2023-04-24 16:01:15 +0300

    xd

--- src/components/Resize.jsx
+++ src/components/Resize.jsx
@@ -1,6 +1,5 @@
 import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
-import "../styles/resize.css";
 
 import { useState } from "react";
 
@@ -22,7 +21,7 @@ export default function Resize() {
 			<style jsx>
 				{`
 					.resize-button {
-						position: absolute;
+						position: fixed;
 						bottom: 8px;
 						right: 8px;
 						outline: 1px solid #72777d;
